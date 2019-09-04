c0d382568b430e3333c57c1d23cebe829a5e6e65
cristian-garcia <cristian.garcia@scmspain.com>
2019-09-04 12:42:13 +0200

    Make properties stable by using Gradle WriteProperties task: it doesn't add a timestamp and makes sure the order is always the same.

--- src/main/kotlin/pl/droidsonroids/gradle/jacoco/testkit/JaCoCoTestKitPlugin.kt
+++ src/main/kotlin/pl/droidsonroids/gradle/jacoco/testkit/JaCoCoTestKitPlugin.kt
@@ -2,20 +2,23 @@ package pl.droidsonroids.gradle.jacoco.testkit
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.tasks.WriteProperties
 import org.gradle.testing.jacoco.plugins.JacocoPlugin.DEFAULT_JACOCO_VERSION
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 import pl.droidsonroids.gradle.jacoco.testkit.Configurations.currentTestRuntime
 import pl.droidsonroids.gradle.jacoco.testkit.Configurations.jacocoRuntime
 import pl.droidsonroids.gradle.jacoco.testkit.Tasks.generateJacocoTestKitProperties
 import pl.droidsonroids.gradle.jacoco.testkit.Tasks.test
+import java.io.File
 
 class JaCoCoTestKitPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         with(project) {
-            configurations.maybeCreate(jacocoRuntime)
-                    .setVisible(false)
-                    .description = "JaCoCo agent for TestKit"
+            val jacocoRuntimeConfiguration = configurations.maybeCreate(jacocoRuntime)
+                .setVisible(false)
+                .setDescription("JaCoCo agent for TestKit")
 
             afterEvaluate {
                 val jacocoVersion = extensions.findByType(JacocoPluginExtension::class.java)?.toolVersion ?: DEFAULT_JACOCO_VERSION
@@ -29,6 +32,7 @@ class JaCoCoTestKitPlugin : Plugin<Project> {
             }
 
             val jacocoTestKitPropertiesTask = tasks.create(generateJacocoTestKitProperties, GenerateJaCoCoTestKitProperties::class.java)
+
             tasks.all {
                 if (it.name == test) {
                     it.dependsOn(jacocoTestKitPropertiesTask)
